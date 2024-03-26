567f8fe60c8773d17f9df41db6f9c14d92172ead
dhananjai-dj <119773597+dhananjai-dj@users.noreply.github.com>
2024-03-26 15:03:48 +0530

    Add files via upload

---
+++ JobScheduler/src/main/java/bean/Lists.java
@@ -0,0 +1,13 @@
+package bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+	public static List<Process> processList = new ArrayList<Process>(); 
+	public static List<Result> resultList = new ArrayList<Result>();
+}
+/*
+ * This class contains two static list which holds the process and result.
+ * These static lists can be used any where in this project.
+*/
